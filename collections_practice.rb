6c4b724634cfaee64a5c19ace2cbd96a6166eed0
Luis Alberto Servin <lservin2015@gmail.com>
2020-02-03 18:48:57 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,15 @@
-def sort
+def sort_array_asc(array)
+  array.sort 
+end 
+
+def sort_array_desc(array)
+  array.sort{|x,y| y <=> x}
+end
+
+def sort_array_char_count(array)
+  array.sort do |a,b|
+    a.length<=> b.length 
+end
+end
+
+def 
