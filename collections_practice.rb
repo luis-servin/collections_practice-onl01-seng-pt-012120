06a068b67682cd4b928f6b951d57fe96fd93ccbd
Luis Alberto Servin <lservin2015@gmail.com>
2020-02-03 18:55:46 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -12,4 +12,45 @@ def sort_array_char_count(array)
 end
 end
 
-def 
+def swap_elements(array)
+  array[1], array[2] = array [2], array[1]
+  array
+end
+
+def reverse_array(array)
+  array.reverse_array
+end
+
+def kesha_maker(array)
+  array.each do |item|
+    item[2] = "$"
+  end
+end
+
+def find_a(array)
+  array.select{|item| item[0] == "a"}
+end
+
+def sum_array(array)
+  sum = 0 
+  array.each do |num|
+    sum += num 
+  end
+  sum
+end
+
+def add_s(array)
+  array.collect do |word|
+    if array [1] == |word|
+      word
+    else
+      word + "s" 
+    end
+  end
+end
+
+
+
+
+
+
